2afdafb70255e6f841f637499aed8c0c55a5de9c
alicjalais <72729449+alicjalais@users.noreply.github.com>
2022-02-05 10:37:52 +0100

    added javascript variables

--- src/app.js
+++ src/app.js
@@ -1,1 +1,4 @@
-
+let searchInput = document.querySelector("#search-input");
+let cityForm = document.querySelector("#search-form");
+let mainCity = document.querySelector(".main-city");
+let temperatureElement = document.querySelector(".temp-container");
