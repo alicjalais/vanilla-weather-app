0eff2ddf9eb716a698943211c5baf6d5ef08995d
alicjalais <72729449+alicjalais@users.noreply.github.com>
2022-02-28 19:31:08 +0100

    added date container and js

--- src/app.js
+++ src/app.js
@@ -2,3 +2,30 @@ let searchInput = document.querySelector("#search-input");
 let cityForm = document.querySelector("#search-form");
 let mainCity = document.querySelector(".main-city");
 let temperatureElement = document.querySelector(".temp-container");
+let dateContainer = document.querySelector("date-container");
+
+function showDate() {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[new Date().getDay()];
+  let hour = new Date().getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minutes = new Date().getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let newDate = `${day}, ${hour}:${minutes}`;
+
+  dateContainer.innerHTML = newDate;
+}
+showDate();
+function showTemp() {}
