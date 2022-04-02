918fc43a0a83c5295ce480b051baabe1f7cd85d3
alicjalais <72729449+alicjalais@users.noreply.github.com>
2022-04-03 01:11:10 +0200

    added a for cycle and icons

--- src/app.js
+++ src/app.js
@@ -3,8 +3,12 @@ let cityForm = document.querySelector("#search-form");
 let mainCity = document.querySelector(".main-city");
 let temperatureElement = document.querySelector(".temp-container");
 let dateContainer = document.querySelector(".date-container");
+let searchButton = document.querySelector(".btn-outline-success");
+let descContainer = document.querySelector(".weather-desc");
+let testContainer = document.querySelector(".test");
+let apiKey = "855c2de2be25508191312e2bfc361fa5";
 
-function showDate() {
+async function showDate() {
   let days = [
     "Sunday",
     "Monday",
@@ -26,21 +30,53 @@ function showDate() {
   let newDate = `${day}, ${hour}:${minutes}`;
 
   dateContainer.innerHTML = newDate;
+  let apiLink = `https://api.openweathermap.org/data/2.5/forecast?lat=35&lon=139&appid=${apiKey}`;
+  let valasz = await axios.get(apiLink);
+
+  for (let index = 0; index < valasz.data.list.length; index += 8) {
+    let dayIndex = new Date(valasz.data.list[index].dt_txt).getDay();
+    testContainer.insertAdjacentHTML(
+      "beforeend",
+      `
+        <div class="col">${days[dayIndex]}</div>
+        `
+    );
+    console.log(new Date(valasz.data.list[index].dt_txt).getDay());
+  }
+  console.log(valasz);
 }
 showDate();
 
-let apiKey = "855c2de2be25508191312e2bfc361fa5";
 let city = "Budapest";
 
 async function searchCity(city) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
   let response = await axios.get(apiUrl);
 
-  console.log(response);
+  console.dir(response.data);
+  /*descContainer.innerHTML = response.data.weather[0].description;*/
+  let icon = "";
+  switch (response.data.weather[0].description) {
+    case "light rain":
+      icon = "wi-day-rain.svg";
+
+      break;
+
+    default:
+      icon = "";
+      break;
+  }
+  console.log(icon);
+  /*descContainer.innerHTML = ` <img src="../weather-icons-master/svg/${icon}" alt="">`;*/
+  descContainer.innerHTML = ` <img src="http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png" alt="">`;
 }
-function searchCity(event) {
-  //event.preventDefault();
-  console.log(event);
+/*function searchCity(event) {
+  event.preventDefault();
+
   searchCity(searchInput.value);
-}
-cityForm.addEventListener("submit", searchCity);
+}*/
+cityForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  searchCity(searchInput.value);
+});
